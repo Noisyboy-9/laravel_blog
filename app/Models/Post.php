860001948b9e75756d30bf7eb8b513a1c089f724
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-08-02 00:26:53 +0430

    Finished section 2

---
+++ app/Models/Post.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Post
+{
+    use HasFactory;
+
+    public string $title;
+    public string $description;
+    public string $body;
+    public string $slug;
+    public string $date;
+
+
+    /**
+     * Post constructor.
+     *
+     * @param string $title
+     * @param string $description
+     * @param string $slug
+     * @param string $date
+     * @param string $body
+     */
+    public function __construct(string $title, string $description, string $slug, string $date, string $body)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->body = $body;
+        $this->slug = $slug;
+        $this->date = $date;
+    }
+
+    /**
+     * find the post from file contents and cache it.
+     *
+     * @throws \Exception
+     */
+    public static function find(string $slug): Post
+    {
+        return cache()->remember(
+            "post.$slug",
+            5,
+            fn() => static::all()->firstWhere('slug', $slug)
+        );
+    }
+
+
+    /**
+     * get all posts in the /resources/posts directory
+     *
+     * @return \Illuminate\Support\Collection
+     * @throws \Exception
+     */
+    public static function all(): Collection
+    {
+        return cache()->rememberForever('posts.all',
+            fn() => collect(File::files(resource_path("/posts")))
+                ->map(fn($document) => YamlFrontMatter::parseFile($document))
+                ->map(fn($file) => new Post(
+                    $file->title,
+                    $file->description,
+                    $file->slug,
+                    $file->date,
+                    $file->body()
+                ))
+        );
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/posts', function () {
+    return view('posts', [
+        'posts' => Post::all()->sortByDesc('date')
+    ]);
+});
+
+Route::get('/posts/{post}', function (string $slug) {
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+})->where('post', '[A-z_\-]+');
+
+
+
+
