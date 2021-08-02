8475a02122e0a35d1ecf0ae0139cbde9ba79bbca
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-08-02 16:21:59 +0430

    Finished episode 15

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,27 +1,15 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="app.css">
-    <title>Posts</title>
-</head>
-<body>
-<h1>All posts</h1>
-@foreach($posts as $post)
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
+<x-layout>
+    @foreach($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
 
-        <div>
-            <p>{{ $post->description }}</p>
-        </div>
-    </article>
-@endforeach
-</body>
-</html>
+            <div>
+                <p>{{ $post->description }}</p>
+            </div>
+        </article>
+    @endforeach
+</x-layout>
