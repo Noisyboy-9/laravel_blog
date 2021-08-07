1e8ef332374f06ab6308532aa19491ad5198e9e4
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-08-07 18:53:57 +0430

    Finished episode 37 and section 5

--- resources/views/components/partials/dropdown.blade.php
+++ resources/views/components/partials/dropdown.blade.php
@@ -6,7 +6,8 @@
     </div>
 
     {{--    links  --}}
-    <div x-show="show" style="display: none" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50">
+    <div x-show="show" style="display: none"
+         class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50 overflow-auto max-h-52">
         {{ $slot }}
     </div>
 </div>
