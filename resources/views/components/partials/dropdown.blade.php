dc0831762945cc169a3a645d36b1cbf2fb804309
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-08-07 18:46:41 +0430

    Finished episode 36

---
+++ resources/views/components/partials/dropdown.blade.php
@@ -0,0 +1,13 @@
+@props(['categories', 'currentCategory'])
+<div x-data="{ show: false }" @click.away="show = false">
+    {{--    trigger --}}
+    <div @click="show = !show">
+        {{ $trigger }}
+    </div>
+
+    {{--    links  --}}
+    <div x-show="show" style="display: none" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50">
+        {{ $slot }}
+    </div>
+</div>
+
