06af7e2ab0a4fb004952093fe1fe57f08043d363
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-08-15 17:41:23 +0430

    Finished episode 58 about understanding basics of mailchimp service

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,20 @@ use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+use MailchimpMarketing\ApiClient;
+
+Route::get('ping', function () {
+    $client = new ApiClient();
+
+    $client->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us5'
+    ]);
+
+    $client->lists->deleteListMemberPermanent('4d1c092209', "7bdfd65cc200ff5c38b875e637f75000");
+    $allMembers = $client->lists->getListMembersInfo('4d1c092209');
+    dd($allMembers);
+});
 
 //static pages
 Route::get('/', [PagesController::class, 'welcome']);
